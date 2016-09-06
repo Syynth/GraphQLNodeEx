99d4bc045cf6f554dd256610712369029bea8405
Kyle Bechtel <kyle.bechtel@careerbuilder.com>
2016-09-06 13:18:03 -0400

    initial commit

---
+++ api.js
@@ -0,0 +1,54 @@
+var express = require('express');
+var app = express();
+
+var person = {
+    "people":[
+        {
+            "friends":[
+                "/people/2/",
+                "/people/3/"
+            ],
+            "username":"steve1",
+            "email": "steve1@steve.com",
+            "last_name": "n1",
+            "id": "1",
+            "first_name":"steven1"
+        },
+        {
+            "friends":[
+                "/people/1/",
+                "/people/3/"
+            ],
+            "username":"steven2",
+            "email": "steve2@steve.com",
+            "last_name": "thomas2",
+            "id": "2",
+            "first_name":"steven2"
+        },
+        {
+            "friends":[
+                "/people/1/",
+                "/people/2/"
+            ],
+            "username":"steven3",
+            "email": "steve3@steve.com",
+            "last_name": "thomas3",
+            "id": "3",
+            "first_name":"steven3"
+        }
+    ]
+}
+
+app.get('/people/:testID/', function (req, res) {
+    console.log(JSON.stringify(person.people[req.params.testID]));
+  res.send(JSON.stringify(person.people[req.params.testID]));
+});
+
+app.get('/people/', function (req, res) {
+    console.log(JSON.stringify(person));
+  res.send(JSON.stringify(person));
+});
+
+app.listen(3001, function () {
+  console.log('Example app listening on port 3001!');
+});
---
+++ graphtest.js
@@ -0,0 +1,96 @@
+  GraphQLList = require("graphql").GraphQLList
+  GraphQLObjectType = require("graphql").GraphQLObjectType
+  GraphQLSchema = require("graphql").GraphQLSchema
+  GraphQLString = require("graphql").GraphQLString
+  GlobalId = require("graphql-relay").GlobalId
+  globalIdField = require("graphql-relay").globalIdField
+  nodeDefinitions = require("graphql-relay").nodeDefinitions
+  DataLoader = require("dataloader")
+  var express = require('express');
+  var graphqlHTTP = require('express-graphql');
+  var fetch = require('node-fetch');
+
+
+  const BASE_URL = 'http://localhost:3001';
+
+  function fetchResponseByURL(relativeURL) {
+    return fetch(`${BASE_URL}${relativeURL}`).then(res => res.json());
+  }
+
+  function fetchPeople() {
+    return fetchResponseByURL('/people/').then(json => json.people);
+  }
+
+  function fetchPersonByURL(relativeURL) {
+    return fetchResponseByURL(relativeURL).then(json => json.person);
+  }
+
+  const personLoader = new DataLoader(
+    urls => Promise.all(urls.map(fetchPersonByURL))
+  );
+
+  const { nodeInterface, nodeField } = nodeDefinitions(
+    globalId => {
+      const {type, id} = fromGlobalId(globalId);
+      if (type === 'Person') {
+        return personLoader.load(`/people/${id}/`);
+      }
+    },
+    object => {
+      if (object.hasOwnProperty('username')) {
+        return 'Person';
+      }
+    }
+  );
+
+  const PersonType = new GraphQLObjectType({
+    name: 'Person',
+    description: 'Somebody that you used to know',
+    fields: () => ({
+      firstName: {
+        type: GraphQLString,
+        resolve: person => person.first_name,
+      },
+      lastName: {
+        type: GraphQLString,
+        resolve: person => person.last_name,
+      },
+      email: {type: GraphQLString},
+      id: globalIdField('Person'),
+      username: {type: GraphQLString},
+      friends: {
+        type: new GraphQLList(PersonType),
+        resolve: person => personLoader.loadMany(person.friends),
+      }
+  }),
+    interfaces: [nodeInterface]
+  });
+
+  const QueryType = new GraphQLObjectType({
+    name: 'Query',
+    description: 'The root of all... queries',
+    fields: () => ({
+      allPeople: {
+        type: new GraphQLList(PersonType),
+        resolve: fetchPeople,
+      },
+      node: nodeField,
+      person: {
+        type: PersonType,
+        args: {
+          id: { type: GraphQLString },
+        },
+        resolve: (root, args) => personLoader.load(`/people/${args.id}/`),
+      }
+    })
+  });
+
+var schema = new GraphQLSchema({
+  query: QueryType,
+});
+
+express()
+  .use('/graphql', graphqlHTTP({ schema: schema, pretty: true }))
+  .listen(3000);
+
+console.log('GraphQL server running on http://localhost:3000/graphql');
