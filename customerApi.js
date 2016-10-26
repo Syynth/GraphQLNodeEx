74781849dfb6991a1d47be13d26fc564079f7ee5
Ben Cochrane <ben.cochrane2112@gmail.com>
2016-10-26 19:46:20 -0400

    Updates customerAPI to return customer objects and addresses

--- customerApi.js
+++ customerApi.js
@@ -1,6 +1,23 @@
 var express = require('express');
 var app = express();
 
+var customers = [
+  {
+    id: 1,
+    name: 'Customer-1',
+  },
+  {
+    id: 2,
+    name: 'Customer-2'
+  },
+];
+
+var senderAddresses = [
+  [], // No customer with ID 0
+  ['email1', 'email2'],
+  ['email3', 'email4']
+];
+
 var person = {
   "data": {
     "email_sender_addresses": [
@@ -9,14 +26,20 @@ var person = {
     ]
   }
 };
+
+app.get(`/customer/:ID`, (req, res) => {
+  res.send(JSON.stringify(customers.find(c => c.id === req.params.ID)));
+});
+
 var i = 0;
 app.get('/customer/:ID/sender-addresses', function (req, res) {
-  var tmp = {};
-  tmp = person;
-  i++;
-  console.log("hit: " + i);
-  console.log(JSON.stringify(tmp));
-  res.send(JSON.stringify(tmp));
+//   var tmp = {};
+//   tmp = person;
+//   i++;
+//   console.log("hit: " + i);
+//   console.log(JSON.stringify(tmp));
+//   res.send(JSON.stringify(tmp));
+  res.send(JSON.stringify(senderAddresses[req.params.ID]));
 });
 
 app.listen(8080, function () {
